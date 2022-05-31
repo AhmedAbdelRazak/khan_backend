a4921be0bba67f770e1b5fdcb713a1acf95f631a
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2022-05-30 17:52:09 -0700

    Adding email to client on reservation update

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -46,11 +46,6 @@ exports.create = (req, res) => {
 		text: `Hi ${order.fullName} - \nYour Ticket was successfully booked \n Please call ${phoneNumber3} if you would like to change anything.\n Thank you for choosing ${BusinessName}`,
 	};
 
-	const smsDataForAdmin = {
-		phone: `${phoneNumber3}`,
-		text: `Hi Admin - \nThere is a new client has filled in the form in your website (Khan Khadija). Please check your dashboard for more details and reach out to the client in case he/she doesn't \nCustomerPhone: +${order.phoneNumber}\nCustomerName: ${order.fullName} \n Dashboard: ${AdminDashboard}`,
-	};
-
 	order.save((err, data) => {
 		if (err) {
 			return res.status(400).json({
@@ -76,29 +71,6 @@ exports.create = (req, res) => {
 			})
 			.then((message) =>
 				console.log(`Your message was successfully sent to ${smsData.phone}`),
-			)
-			.catch((err) => console.log(err));
-
-		const smsForAdmin = new SMS(smsDataForAdmin);
-		smsForAdmin.save((err, data) => {
-			if (err) {
-				return res.status(400).json({
-					err: "Error in sms creation",
-				});
-			}
-			console.log(data, "sms saved in the data base");
-		});
-
-		orderStatusSMS.messages
-			.create({
-				body: smsDataForAdmin.text,
-				from: "+19094884148",
-				to: smsDataForAdmin.phone,
-			})
-			.then((message) =>
-				console.log(
-					`Your message was successfully sent to ${smsDataForAdmin.phone}`,
-				),
 			)
 			.catch((err) => console.log(err));
 
@@ -472,33 +444,7 @@ exports.updateOrderStatus = (req, res) => {
 				to: req.order.scheduledByUserEmail,
 				from: "noreply@infinite-apps.com",
 				subject: `Khan Khadija | Reservation Update`,
-				html: `<div>Hi ${req.order.fullName}, </div>
-        <br />
-        <p>There is an update to your reservation.</p>
-        <br />
-              <h4>Reservation status: ${req.body.status}</h4>
-
-              Once we have another update, we will let you know.
-
-              <h4> <div>Thank you for choosing <a href=${BusinessWebsite}> ${BusinessName}</a>.</div><h4/>
-
-			  <br />
-			  <br />
-			   </div>
-			   Kind and Best Regards,  <br />
-						   ${BusinessName} support team <br />
-						   Contact Email: ${defaultEmail} <br />
-						   Phone#: ${phoneNumber3} <br />
-						   Address:  ${shopAddress}  <br />
-						   &nbsp;&nbsp; <img src=${shopLogo} alt=${BusinessName} style="height:80px;width:250px;"  />
-						   <br />
-						   <p>
-						   <strong>${BusinessName}</strong>
-							</p>
-							</div>
-
-
-							<html lang="en">
+				html: `	<html lang="en">
 							<head>
 								<meta charset="UTF-8" />
 								<meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -626,34 +572,6 @@ exports.updateOrderStatus = (req, res) => {
 
 			sgMail.send(emailData3);
 			res.json(order);
-
-			// const smsDataForAdmin = {
-			// 	phone: `+19512591528`,
-			// 	text: `Hi Admin - \nThere is an update in client's reservation (Khan Khadija). Please check your dashboard for more details and reach out to the client in case he/she doesn't \nCustomerPhone: +${req.order.scheduledByUserEmail}\nCustomerName: ${req.order.fullName} \n Dashboard: ${AdminDashboard}`,
-			// };
-
-			// const smsForAdmin = new SMS(smsDataForAdmin);
-			// smsForAdmin.save((err, data) => {
-			// 	if (err) {
-			// 		return res.status(400).json({
-			// 			err: "Error in sms creation",
-			// 		});
-			// 	}
-			// 	console.log(data, "sms saved in the data base");
-			// });
-
-			// orderStatusSMS.messages
-			// 	.create({
-			// 		body: smsDataForAdmin.text,
-			// 		from: "+19094884148",
-			// 		to: smsDataForAdmin.phone,
-			// 	})
-			// 	.then((message) =>
-			// 		console.log(
-			// 			`Your message was successfully sent to ${smsDataForAdmin.phone}`,
-			// 		),
-			// 	)
-			// 	.catch((err) => console.log(err));
 		},
 	);
 };
@@ -679,9 +597,13 @@ exports.updateOrderScheduleDate = (req, res) => {
 exports.updateReservation = (req, res) => {
 	const order = req.order;
 	// const orderDetails = req.body.orderDetails;
+	// console.log(req.body.scheduledByUserEmail, "req.body");
+	// console.log(req.order, "req.order");
 
-	console.log(req.body.scheduledByUserEmail, "req.body");
-	console.log(req.order, "req.order");
+	const smsData = {
+		phone: `+${req.body.phoneNumber}`,
+		text: `Hi ${req.body.fullName} - \nYour Ticket was updated \n Please call ${phoneNumber3} or check your email (${req.body.scheduledByUserEmail}) to confirm.\n Thank you for choosing ${BusinessName}`,
+	};
 
 	order.scheduledByUserEmail = req.body.scheduledByUserEmail;
 	order.totalAmount = req.body.totalAmount;
@@ -714,6 +636,174 @@ exports.updateReservation = (req, res) => {
 				error: err,
 			});
 		}
+		const sms = new SMS(smsData);
+		sms.save((err, data) => {
+			if (err) {
+				console.log(err, "from creating SMSData");
+				return res.status(400).json({
+					err: "Error in sms creation",
+				});
+			}
+			console.log(data, "sms saved in the data base");
+		});
+
+		orderStatusSMS.messages
+			.create({
+				body: smsData.text,
+				from: "+19094884148",
+				to: smsData.phone,
+			})
+			.then((message) =>
+				console.log(`Your message was successfully sent to ${smsData.phone}`),
+			)
+			.catch((err) => console.log(err));
+
+		const FormSubmittionEmail = {
+			to: order.scheduledByUserEmail,
+			from: fromEmail,
+			subject: `${BusinessName} - Reservation Update`,
+			html: `
+				<html lang="en">
+				<head>
+					<meta charset="UTF-8" />
+					<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			
+					<link
+						rel="stylesheet"
+						href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+						integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+						crossorigin="anonymous"
+					/>
+			
+					<title></title>
+				</head>
+				<style>
+				body {
+					background-color: #1e467d;
+					color: white;
+					margin: 10px;
+					justify-content: center;
+					font-weight: bold;
+					padding: 10px;
+					border-radius: 3px;
+	
+				}
+		
+				h5 {
+					font-weight: bold;
+					font-size: 1.2rem;
+				}
+		
+				.mainDiv {
+					padding: 20px;
+				}
+		
+				a {
+					color: #fcb63e;
+				}
+		
+				.logoWrapper {
+					/* border: 1px red solid; */
+					background-color: #fcb63e;
+					border-radius: 10px;
+					margin: auto 10px;
+				}
+		
+				.bookingInfoWrapper {
+					background-color: antiquewhite;
+					padding: 30px;
+					color: black;
+					border-radius: 15px;
+					margin: 10px auto;
+				}
+		
+				h4 {
+					color: #1e467d !important;
+					font-weight: bolder;
+				}
+		
+				@media (max-width: 1000px) {
+					body {
+						background-color: #1e467d;
+						color: white;
+						margin: 4px;
+						padding: 7px;
+					}
+					.imgStyling {
+						height: 100px !important;
+						width: 300px !important;
+					}
+		
+					.bookingInfoWrapper {
+						padding: 20px;
+					}
+				}
+			</style>
+				<body>
+					<div class="col-md-5 mx-auto mainDiv">
+						<div class="mx-auto text-center logoWrapper mb-3">
+							<img
+								src="https://res.cloudinary.com/infiniteapps/image/upload/v1652480319/khankhadija/LogoSideBar_YusufSidebar_gfw40c.png"
+								alt="${BusinessName}"
+								class="imgStyling"
+								style="height: 40%; width: 40%"
+							/>
+						</div>
+			
+						<h5 style="position: relative">Hi ${req.body.fullName}</h5>
+						<div class="mb-2">
+							Thank you for choosing
+							<a href="${BusinessWebsite}"> ${BusinessName}</a>.
+						</div>
+						If you would like to change your ticket info, Please call ${phoneNumber3}
+						and our support team will help you.
+						<br />
+						<div class="bookingInfoWrapper">
+							<h4>Your Booking Info:</h4>
+							Phone: ${req.body.phoneNumber}
+							<br />
+							Scheduled Date: ${new Date(req.body.scheduledDate).toLocaleDateString()}
+							<br />
+							Tickets Count (Adults): ${req.body.quantity}
+							<br />
+							Tickets Count (Children): ${req.body.quantity_Children}
+							<br />
+							Bus Station Address: ${req.body.chosenBusStation.address}
+							<br />
+							Bus Station Time: ${req.body.chosenBusStationTime}
+							<br />
+							Chosen Package/Ticket: ${req.body.chosenService_Package}
+							<br />
+							Options Added: ${
+								Number(req.body.option1Count) +
+								Number(req.body.option2Count) +
+								Number(req.body.option3Count) +
+								Number(req.body.option4Count)
+							}
+							<br />
+							Total Amount: ${req.body.totalAmount} L.E
+							<br />
+						</div>
+						<br />
+						Kind and Best Regards, <br />
+						${BusinessName} support team <br />
+						Contact Email: ${defaultEmail} <br />
+						Phone#: ${phoneNumber3} <br />
+						Address: ${shopAddress} <br />
+						&nbsp;&nbsp;
+			
+						<br />
+						<p>
+							<strong>${BusinessName}</strong>
+						</p>
+					</div>
+				</body>
+			</html>
+			`,
+		};
+		sgMail.send(FormSubmittionEmail);
+
 		res.json(data);
 	});
 };
